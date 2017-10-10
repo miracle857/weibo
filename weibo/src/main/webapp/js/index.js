be096117aa4a23610a34c09c0cdb3f937098df6e
mxh984511321 <984511321@QQ.com>
2017-10-10 17:33:26 +0800

    发布微博功能 完成了一个架子

--- weibo/src/main/webapp/js/index.js
+++ weibo/src/main/webapp/js/index.js
@@ -16,6 +16,7 @@ var body = "<div class='weibo-body' style='display:none' id='test'>"
 		+ "</div>"
 		+ "<div class='area'>"
 		+ content
+		+ "<br><br>"
 		+ "</div>"
 		+ "</div>"
 		+ "<div class='menu'>"
@@ -27,6 +28,7 @@ var body = "<div class='weibo-body' style='display:none' id='test'>"
 		+ "</ul>" + "</div>" + "</div>" + "</div>";
 
 // 监听textarea内容文本变化，确定是否开放发布按钮
+$(".btn-reply").attr({"disabled":"disabled"});
 $("#reply-content").on('input', function() {
     if ($.trim($("#reply-content").val()).length == 0) {
         $(".btn-reply").attr({"disabled":"disabled"});
@@ -56,13 +58,45 @@ $(".btn-reply").click(function() {
 		datatype : "json",
 		success : function(data){
 			// 3.日期处理，并展示内容
-			console.log(data.body);
-			$("#container").prepend(body);
+			$("#container").prepend(getWeibo(data));
 			$("#test").show("slow");
+			$("#reply-content").val('');
 		},
 		error : function(){
 			
 		}
 	});
-	
 });
+
+function getWeibo(data){
+	var time = FormatDateTime(data.body.publishTime);
+	var name = data.body.userNickname;
+	var content = data.body.content;
+	var img = data.body.userHeadimg;
+	var body = "<div class='weibo-body' style='display:none' id='test'>"
+		+ "<div class='inner'>"
+		+ "<div class='head-img'>"
+		+ "<img src='/img/logo.jpg' alt='none' class='img-circle body-img'>"
+		+ "</div>"
+		+ "<div class='other'>"
+		+ "<div class='_username'>"
+		+ name
+		+ "</div>"
+		+ "<div class='time'>"
+		+ time
+		+ "</div>"
+		+ "<div class='area'>"
+		+ content
+		+ "<br><br>"
+		+ "</div>"
+		+ "</div>"
+		+ "<div class='menu'>"
+		+ "<ul>"
+		+ "<li> <a href=''><i class='fa fa-star-o'></i><i class='fa fa-star'></i>收藏</a></li>"
+		+ "<li> <a href=''><i class='fa fa-share'></i>转发</a></li>"
+		+ "<li> <a href=''><i class='fa fa-commenting-o'></i> 回复</a></li>"
+		+ "<li> <a href=''><i class='fa fa-thumbs-o-up'></i><i class='fa fa-thumbs-up'></i>点赞</a></li>"
+		+ "</ul>" + "</div>" + "</div>" + "</div>";
+	
+	return body;
+}
