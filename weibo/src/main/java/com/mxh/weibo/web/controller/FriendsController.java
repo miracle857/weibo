253bc988cca43ce7dcf11708309a906f0ea21053
Administrator <Administrator@USERCHI-SGL6R6B>
2018-04-24 22:50:55 +0800

    4.24

--- weibo/src/main/java/com/mxh/weibo/web/controller/FriendsController.java
+++ weibo/src/main/java/com/mxh/weibo/web/controller/FriendsController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mxh.weibo.common.PaginatedList;
 import com.mxh.weibo.common.Pagination;
@@ -27,12 +28,35 @@ public class FriendsController {
     private UserService userService;
 
     @RequestMapping("/fans")
+    @ResponseBody
     public ResponsePageVo getFans(UserCriterua vo,HttpSession session) {
     	ResponsePageVo res = new ResponsePageVo();
     	
     	User user = (User)session.getAttribute("user");
     	try {
 			PaginatedList<UserVo> fans = userService.getFans(user.getUuid(), vo);
+			res.setRows(fans.getResult());
+			
+	    	Pagination pagination = fans.getPagination();
+	    	res.setPage(pagination.getPage());
+	    	res.setTotal(pagination.getTotal());
+			res.setPageCount(pagination.getTotalPage());
+			res.setPageSize(pagination.getPagesize());
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+		}
+    	return res;
+    }
+    
+    @RequestMapping("/follower")
+    @ResponseBody
+    public ResponsePageVo getFollower(UserCriterua vo,HttpSession session) {
+    	ResponsePageVo res = new ResponsePageVo();
+    	
+    	User user = (User)session.getAttribute("user");
+    	try {
+			PaginatedList<UserVo> fans = userService.getFollower(user.getUuid(), vo);
 			res.setRows(fans.getResult());
 			
 	    	Pagination pagination = fans.getPagination();
